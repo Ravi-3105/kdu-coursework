a30d7e2598708927c44c5b82b61fba4a06fbca00
Ravi <raviranjan.maurya@kickdrum.tech.com>
2024-02-15 00:39:01 +0530

    Frontend Homework 7 first commit

---
+++ frontend/homework/7/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new socketIo.Server(server, {
+  cors: {
+    origin: '*'
+  },
+});
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: "Hello World",
+  });
+});
+
+io.on("connection", (socket) => {
+  console.log("connected");
+
+  socket.on("message",(payload) =>{
+    console.log("Payload",payload);
+    io.except(socket.id).emit('new-message',payload);
+  })
+});
+
+server.listen(3000, () => {
+  console.log("Server started");
+});
